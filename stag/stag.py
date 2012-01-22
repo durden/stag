0fcef500aaf15d6930b8588c3a1717bcc5c7c656
Luke Lee <durdenmisc@gmail.com>
2012-01-22 13:47:21 -0600

    Start of arg parsing, looking through content directory

--- stag/stag.py
+++ stag/stag.py
@@ -4,10 +4,26 @@
 Module to provide creation of a static HTML website
 """
 
+import os
+import argparse
 
-def main():
+
+def main(content_dir):
     """main"""
 
+    for root, dirs, files in os.walk(content_dir):
+        for dir_name in dirs:
+            if dir_name.startswith('.'):
+                continue
+
+        for file_name in files:
+            if file_name.startswith('.'):
+                continue
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Static website generator')
+    parser.add_argument('content_dir', type=str,
+                        help='Full path to main directory of markdown content')
+    args = parser.parse_args()
+
+    main(args.content_dir)
