88a63274943de38e8abcebca5ef774285bdb74db
Luke Lee <durdenmisc@gmail.com>
2012-01-31 07:23:27 -0600

    Add basic setup.py for pip/easy_install installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Stag',
+      version='0.1',
+      description='Framework for creating Web APIs in Python',
+      author='Luke Lee',
+      author_email='durdenmisc@gmail.com',
+      url='http://github.com/durden/stag',
+      packages=['stag'],
+    )
